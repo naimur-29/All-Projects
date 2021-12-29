c7fd69b552fe8bf379cc4b3dd0773c35b9523df1
naimur-29 <skylike2904@gmail.com>
2021-12-30 00:40:46 +0600

    update

---
+++ Python/tic tac toe/tic_tac_toe.py
@@ -0,0 +1,68 @@
+from os import system, name
+
+box_0 = [" ", " ", " "]
+box_1 = [" ", " ", " "]
+box_2 = [" ", " ", " "]
+box = [box_0, box_1, box_2]
+count = 0
+user = 0
+won = False
+inputs = list()
+inp_dict = ("00","01","02","10","11","12","20","21","22")
+
+def call_screen():
+    print(box_0[0], box_0[1], box_0[2])
+    print(box_1[0], box_1[1], box_1[2])
+    print(box_2[0], box_2[1], box_2[2])
+    print()
+
+def clear():
+    if name == 'nt':
+        _ = system("cls")
+    else:
+        _ = system("clear")
+
+def use_input():
+    if count%2 != 0:
+        box[int(inp_dict[user-1][0])][int(inp_dict[user-1][1])] = "O"
+    else:
+        box[int(inp_dict[user-1][0])][int(inp_dict[user-1][1])] = "X"
+
+def print_result():
+    if count%2 != 0 and won == True:
+        print(player_1, "who chose 'O' wins...")
+    elif count%2 == 0 and won == True:
+        print(player_2, "who chose 'X' wins...")
+    else:
+        print('Nobody wins...')
+
+
+
+clear()
+player_1 = input("Enter your name: ")
+player_2 = input("Enter your name: ")
+
+while won == False:
+    count += 1
+    if count == 10:
+        break
+    clear()
+    call_screen()
+    print("WELCOME TO TIC TAC TOE")
+    user = int(input("Enter the position (1 - 9): "))
+    while 1 == 1:
+        if user <= 0 or user > 9:
+            user = int(input("Wrong position...\nEnter the correct position: "))
+        else:
+            if user not in inputs:
+                break
+            else:
+                user = int(input("Wrong position...\nEnter the correct position: "))
+    inputs.append(user)
+    use_input()
+    if (box_0[0] == box_0[1] == box_0[2] != ' ') or (box_1[0] == box_1[1] == box_1[2] != ' ') or (box_2[0] == box_2[1] == box_2[2] != ' ') or (box_0[0] == box_1[0] == box_2[0] != ' ') or (box_0[1] == box_1[1] == box_2[1] != ' ') or (box_0[2] == box_1[2] == box_2[2] != ' ') or (box_0[0] == box_1[1] == box_2[2] != ' ') or (box_0[2] == box_1[1] == box_2[0] != ' '):
+        won = True
+
+clear()
+call_screen()
+print_result()
