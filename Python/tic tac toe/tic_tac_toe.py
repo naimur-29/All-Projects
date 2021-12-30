781f3d5c1949419dff5d85f35d9ed5a0ad3c28c7
naimur-29 <skylike2904@gmail.com>
2021-12-30 10:28:10 +0600

    update

--- Python/tic tac toe/tic_tac_toe.py
+++ Python/tic tac toe/tic_tac_toe.py
@@ -1,4 +1,5 @@
 from os import system, name
+from time import sleep
 
 box_0 = [" ", " ", " "]
 box_1 = [" ", " ", " "]
@@ -22,25 +23,21 @@ def clear():
     else:
         _ = system("clear")
 
+def print_result():
+    if count%2 != 0 and won == True:
+        print("player 1 who chose 'O' wins...")
+    elif count%2 == 0 and won == True:
+        print("player 2 who chose 'X' wins...")
+    else:
+        print('Nobody wins...')
+
 def use_input():
     if count%2 != 0:
         box[int(inp_dict[user-1][0])][int(inp_dict[user-1][1])] = "O"
     else:
         box[int(inp_dict[user-1][0])][int(inp_dict[user-1][1])] = "X"
 
-def print_result():
-    if count%2 != 0 and won == True:
-        print(player_1, "who chose 'O' wins...")
-    elif count%2 == 0 and won == True:
-        print(player_2, "who chose 'X' wins...")
-    else:
-        print('Nobody wins...')
 
-
-
-clear()
-player_1 = input("Enter your name: ")
-player_2 = input("Enter your name: ")
 
 while won == False:
     count += 1
@@ -49,20 +46,25 @@ while won == False:
     clear()
     call_screen()
     print("WELCOME TO TIC TAC TOE")
-    user = int(input("Enter the position (1 - 9): "))
     while 1 == 1:
-        if user <= 0 or user > 9:
-            user = int(input("Wrong position...\nEnter the correct position: "))
-        else:
-            if user not in inputs:
-                break
-            else:
-                user = int(input("Wrong position...\nEnter the correct position: "))
-    inputs.append(user)
+        try:
+            user = int(input("Enter the position (1 - 9): "))
+            while 1 == 1:
+                if user <= 0 or user > 9:
+                    user = int(input("Wrong position...\nEnter the correct position: "))
+                else:
+                    if user not in inputs:
+                        break
+                    else:
+                        user = int(input("Wrong position...\nEnter the correct position: "))
+            inputs.append(user)
+            break
+        except:
+            print("Wrong position...")
     use_input()
     if (box_0[0] == box_0[1] == box_0[2] != ' ') or (box_1[0] == box_1[1] == box_1[2] != ' ') or (box_2[0] == box_2[1] == box_2[2] != ' ') or (box_0[0] == box_1[0] == box_2[0] != ' ') or (box_0[1] == box_1[1] == box_2[1] != ' ') or (box_0[2] == box_1[2] == box_2[2] != ' ') or (box_0[0] == box_1[1] == box_2[2] != ' ') or (box_0[2] == box_1[1] == box_2[0] != ' '):
         won = True
-
 clear()
 call_screen()
 print_result()
+sleep(10)
