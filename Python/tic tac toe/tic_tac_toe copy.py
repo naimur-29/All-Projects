4aa1ea7864b4a692df7b96ded48a63e5f81b970b
naimur-29 <skylike2904@gmail.com>
2022-01-03 14:03:32 +0600

    update

--- Python/tic tac toe/tic_tac_toe copy.py
+++ Python/tic tac toe/tic_tac_toe copy.py
@@ -21,7 +21,7 @@ def call_screen():
     print(" ", box_2[0], "|", box_2[1], "|", box_2[2])
     print()
 
-def call_screen_animated():
+def call_screen_animted():
     clear()
     print(" ", box_0[0], "|", box_0[1], "|", box_0[2])
     sleep(0.01)
@@ -53,7 +53,7 @@ def print_result_multi():
         print(winner[0:i])
         if i == len(winner):
             sleep(0.5)
-            call_screen_animated()
+            call_screen_animted()
             print(winner)
         else:
             sleep(0.05)
@@ -63,14 +63,14 @@ def print_result_single():
     if count%2 != 0 and won == True:
         winner = "BOT WHO CHOSE 'O' IS THE WINNER!!"
     elif count%2 == 0 and won == True:
-        winner = "PLAYER WHO CHOSE 'X' IS THE WINNER"
+        winner = "PLAYER 2 WHO CHOSE 'X' IS THE WINNER"
     else:
         winner = "UNFORTUNATELY IT'S A DRAW"
     for i in range(len(winner)+1):
         print(winner[0:i])
         if i == len(winner):
-            sleep(1)
-            call_screen_animated()
+            sleep(0.5)
+            call_screen_animted()
             print(winner)
         else:
             sleep(0.001)
@@ -190,29 +190,52 @@ def t2Bot3(inp):
             return 5
     elif inputs[len(inputs)-5] == 5:
         if inputs[len(inputs)-6] == 1:
+            if inp == 2:
+                return 4
+            else:
+                return 2
+        elif inputs[len(inputs)-6] == 9:
             if inp == 6:
                 return 8
             else:
                 return 6
-        elif inputs[len(inputs)-6] == 9:
-            if inp == 8:
-                return 4
-            else:
-                return 8
         else:
-            if inp == 2:
-                return 8
-            elif inp == 4:
-                return 6
-            elif inp == 6:
-                return 4
-            elif inp == 8:
-                return 2
-            elif inp == 1:
-                return 9
+            if inputs[len(inputs)-5] == 2:
+                if inp == 4:
+                    return 6
+                elif inp == 9:
+                    return 1
+                elif inp == 1:
+                    return 9
+                else:
+                    return 4
+            if inputs[len(inputs)-5] == 4:
+                if inp == 2:
+                    return 8
+                elif inp == 9:
+                    return 1
+                elif inp == 1:
+                    return 9
+                else:
+                    return 2
+            if inputs[len(inputs)-5] == 6:
+                if inp == 2:
+                    return 8
+                elif inp == 9:
+                    return 1
+                elif inp == 1:
+                    return 9
+                else:
+                    return 2
             else:
-                return 1
-            
+                if inp == 4:
+                    return 6
+                elif inp == 9:
+                    return 1
+                elif inp == 1:
+                    return 9
+                else:
+                    return 4
     else:
         if inputs[len(inputs)-3] == 8:
             return 4
@@ -261,7 +284,7 @@ while replay == '':
         clear()
     if initiate != '':
         break
-    call_screen_animated()
+    call_screen_animted()
     gameMode = input("[S] - SINGLE PLAYER MODE\n[M] - MULTI-PLAYER MODE\n")
     while 1 == 1:
         if gameMode == 's' or gameMode == 'S':
@@ -277,27 +300,27 @@ while replay == '':
                 count += 1
                 if count == 10:
                     break
-                call_screen_animated()
+                call_screen()
                 if count == 1:
                     user = 7
                     use_input()
-                    call_screen_animated()
+                    call_screen()
                 elif count == 3:
                     user = t2Bot1(user)
                     use_input()
-                    call_screen_animated()
+                    call_screen()
                 elif count == 5:
                     user = t2Bot2(user)
                     use_input()
-                    call_screen_animated()
+                    call_screen()
                 elif count == 7:
                     user = t2Bot3(user)
                     use_input()
-                    call_screen_animated()
+                    call_screen()
                 elif count == 9:
                     user = t2Bot4(user)
                     use_input()
-                    call_screen_animated()
+                    call_screen()
             
                 if (box_0[0] == box_0[1] == box_0[2] != ' ') or (box_1[0] == box_1[1] == box_1[2] != ' ') or (box_2[0] == box_2[1] == box_2[2] != ' ') or (box_0[0] == box_1[0] == box_2[0] != ' ') or (box_0[1] == box_1[1] == box_2[1] != ' ') or (box_0[2] == box_1[2] == box_2[2] != ' ') or (box_0[0] == box_1[1] == box_2[2] != ' ') or (box_0[2] == box_1[1] == box_2[0] != ' '):
                     won = True
@@ -343,7 +366,7 @@ while replay == '':
                 count += 1
                 if count == 10:
                     break
-                call_screen_animated()
+                call_screen()
                 while 1 == 1:
                     try:
                         user = int(input("Enter the position (1 - 9): "))
